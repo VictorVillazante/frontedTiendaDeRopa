2e9347c855e413d68b774058da8734b99e48fafb
PorcelJhoan <porcel.pena.jhoan@gmail.com>
2022-04-20 15:29:25 -0400

    subiendo cambios login

--- src/app/components/create-account-user/create-account-user.component.ts
+++ src/app/components/create-account-user/create-account-user.component.ts
@@ -1,23 +1,67 @@
 import { Component, OnInit } from '@angular/core';
 import { Usuario } from 'src/app/models/Usuario';
+import { AuthService } from 'src/app/services/auth.service';
+import { TokenService } from 'src/app/services/token.service';
+import { LoginUsuario } from 'src/app/models/login-usuario';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-create-account-user',
   templateUrl: './create-account-user.component.html',
   styleUrls: ['./create-account-user.component.css']
 })
 export class CreateAccountUserComponent implements OnInit {
+  loginUsuario!: LoginUsuario;
+  nombreUsuario = '';
+  password = '';
   usuarioNuevo:Usuario={id_usuario:null,nombre:"",apellido:"",email:"",password:""};
+  isLogged = false;
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private tokenService: TokenService,
+    private router: Router
+  ) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.testLogged();
   }
-  registrarNuevoUsuario(){
-    if(this.usuarioNuevo.nombre!="" && this.usuarioNuevo.apellido!="" && this.usuarioNuevo.email!="" && this.usuarioNuevo.password!=""){
-      window.alert("Usuario registrado correctamente");
-    }else{
-      window.alert("No deje los campos vacios");
-    }
+
+  ionViewWillEnter() {
+    this.testLogged();
+    this.vaciar();
   }
+
+
+  onLogin() {
+    this.loginUsuario = new LoginUsuario(this.nombreUsuario, this.password);
+    this.authService.login(this.loginUsuario).subscribe(
+      data => {
+        this.tokenService.setToken(data.token);
+        this.isLogged = true;
+        this.router.navigate(['/']);
+      },
+      err => {
+        console.log(err.error.message);
+      }
+    );
+  }
+
+  vaciar() {
+    this.nombreUsuario = '';
+    this.password = '';
+  }
+
+ 
+
+  logOut(): void {
+    this.tokenService.logOut();
+    this.isLogged = false;
+    this.vaciar();
+  }
+
+  testLogged(): void {
+    this.isLogged = this.tokenService.getToken() != null;
+  }
+
 
 }
